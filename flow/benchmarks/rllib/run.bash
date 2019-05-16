dbed08a22e915d2dd23685a6376b5e40f215eb9b
syuntoku14 <syuntoku14@keio.jp>
2019-05-16 22:54:36 +0000

    Modify for loop merge exp

--- flow/benchmarks/rllib/run.bash
+++ flow/benchmarks/rllib/run.bash
@@ -1,1 +1,1 @@
-python ppo_runner_loop.py --benchmark_name multi_loop_merge --exp_tag loop_merge --num_cpus 2 --num_rollouts 1
+python ppo_runner_loop.py --benchmark_name multi_loop_merge --exp_tag loop_merge --num_cpus 31 --num_rollouts 30
